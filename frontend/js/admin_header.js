8933d0e55866fd075d56594a61f0941c3ef88127
Jaewonnnn <yjw970710@gmail.com>
2024-08-31 14:17:12 +0900

    [Style]: admin_route page 구현 #24

--- frontend/js/admin_header.js
+++ frontend/js/admin_header.js
@@ -1,7 +1,7 @@
 class Headers extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="/css/reset.css">
       <link rel="stylesheet" href="/css/header.css">
@@ -25,42 +25,45 @@ class Headers extends HTMLElement {
   }
 }
 
-customElements.define('admin-header-component', Headers);
+customElements.define("admin-header-component", Headers);
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   headerMenu();
 });
 
 function headerMenu() {
-  const shadowRoot = document.querySelector('header-component').shadowRoot;
-  const menuItems = shadowRoot.querySelectorAll('#header_menu > li');
+  const shadowRoot = document.querySelector("header-component").shadowRoot;
+  const menuItems = shadowRoot.querySelectorAll("#header_menu > li");
 
-  menuItems.forEach(item => {
-    item.addEventListener('click', (event) => {
+  menuItems.forEach((item) => {
+    item.addEventListener("click", (event) => {
       event.stopPropagation();
-      const currentItem = shadowRoot.querySelector('#header_menu > li.selector');
+      const currentItem = shadowRoot.querySelector(
+        "#header_menu > li.selector"
+      );
       if (currentItem) {
-        currentItem.classList.remove('selector');
+        currentItem.classList.remove("selector");
       }
-      item.classList.add('selector');
-
+      item.classList.add("selector");
     });
   });
 
   // 로컬 스토리지에서 저장된 선택된 메뉴 항목을 불러와서 설정합니다.
-  const selectedMenu = localStorage.getItem('selectedMenu');
+  const selectedMenu = localStorage.getItem("selectedMenu");
   if (selectedMenu) {
-    const selectedItem = Array.from(menuItems).find(item => {
-      const link = item.querySelector('a');
-      return link && link.getAttribute('href') === selectedMenu;
+    const selectedItem = Array.from(menuItems).find((item) => {
+      const link = item.querySelector("a");
+      return link && link.getAttribute("href") === selectedMenu;
     });
 
     if (selectedItem) {
-      const currentItem = shadowRoot.querySelector('#header_menu > li.selector');
+      const currentItem = shadowRoot.querySelector(
+        "#header_menu > li.selector"
+      );
       if (currentItem) {
-        currentItem.classList.remove('selector');
+        currentItem.classList.remove("selector");
       }
-      selectedItem.classList.add('selector');
+      selectedItem.classList.add("selector");
     }
   }
 }
