654f807bb7d643abb516cf35ea20bf670388aca4
Jaewonnnn <yjw970710@gmail.com>
2024-09-01 13:51:27 +0900

    [HOTFIX]: UserController 오류 수정

--- backend/src/main/java/com/unibus/user/controller/UserController.java
+++ backend/src/main/java/com/unibus/user/controller/UserController.java
@@ -3,19 +3,5 @@ package com.unibus.user.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-
 public class UserController {
-
-    private final UserService userService;
-
-    @GetMapping("/users")
-    public List<Member> getUserList() {
-        return userService.getUserList();
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return "hello";
-    }
-
 }
