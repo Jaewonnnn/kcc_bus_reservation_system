8730982a9125225c515dd13336d2f010b95fda0f
WonSeungUn <dnjstmddjs12@naver.com>
2024-09-01 22:50:45 +0900

    [Feature] ID 중복검사, 회원정보 수정, 회원정보 조회, 회원 탈퇴 API 기능 만들었습니다. #47

--- backend/src/main/java/com/unibus/user/service/UserService.java
+++ backend/src/main/java/com/unibus/user/service/UserService.java
@@ -1,4 +1,14 @@
 package com.unibus.user.service;
 
+import com.unibus.user.domain.Member;
+
+import java.util.List;
+
+import org.springframework.security.config.annotation.web.configurers.UrlAuthorizationConfigurer.StandardInterceptUrlRegistry;
+
 public interface UserService {
+    public Member userInfo(String memberId);
+    public Integer updateMember(Member member);
+    public Integer updateWithdraw(Member member);
+    public Boolean checkMemberId(String memberId);
 }
--- backend/src/main/java/com/unibus/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/unibus/user/service/UserServiceImpl.java
@@ -1,7 +1,41 @@
 package com.unibus.user.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    private final UserMapper userMapper;
+
+    @Override
+    public Member userInfo(String memberId) {
+        return userMapper.getMemberByMemberId(memberId);
+    }
+
+    @Override
+    @Transactional
+    public Integer updateMember(Member member) {
+        return userMapper.updateMemberByMemberId(member);
+    }
+
+    @Override
+    @Transactional
+    public Integer updateWithdraw(Member member) {
+        Member member2 = userMapper.getMemberByMemberId(member.getMemberId());
+        if(member2.getMemberPass().equals(member.getMemberPass())) {
+            return userMapper.withdrawMemberByMemberPass(member);
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public Boolean checkMemberId(String memberId) {
+        return userMapper.selectDuplicationByMemberId(memberId) == 1 ? false : true;
+    }
+
+
+
 }
